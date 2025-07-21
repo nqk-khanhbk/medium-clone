d953705accde927e72d2a24caa1fa624f30fbdf8
nqk-khanhbk <khanhbk0102@gmail.com>
2025-07-21 12:12:54 +0700

    Các chức năng của phần bài viết

---
+++ prisma/migrations/20250722024821_add_favorites_count/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Article" ADD COLUMN     "favoritesCount" INTEGER NOT NULL DEFAULT 0;
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,12 +3,13 @@ import { PrismaService } from './services/prisma.service'
 import { HashingService } from './services/hashing.service'
 import { TokenService } from './services/token.services'
 import { JwtModule } from '@nestjs/jwt'
+import { ArticleModule } from './routes/article/article.module';
 
 const sharedServices = [PrismaService, HashingService, TokenService]
 @Global()
 @Module({
   providers: sharedServices,
   exports: sharedServices,
-  imports: [JwtModule],
+  imports: [JwtModule, ArticleModule],
 })
 export class SharedModule {}
